f484df3d9cd56db815d27c964338d4af75defe8d
Heitor da Costa <heitor2000costa@gmail.com>
2022-02-04 13:05:08 -0300

    adicionado o dia1 do bloco4

---
+++ modulo1/bloco4-javascript/dia1/scripts.js
@@ -0,0 +1,85 @@
+/*1)Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b)*/
+
+/*let a = 34;
+let b = 28;
+let c = 75;*/
+
+/*let soma = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+let resultados = [soma, subtracao, multiplicacao, divisao, modulo];
+
+console.log(resultados);*/
+//2)Faça um programa que retorne o maior de dois números. 
+//Defina no começo do programa duas constantes com os valores que serão comparados.
+
+/*if (a > b){
+    console.log("a é maior que b")
+} else {
+    console.log("b é maior que a")
+}*/
+
+//3) Faça um programa que retorne o maior de três números. Defina no começo 
+//do programa três constantes com os valores que serão comparados.
+/*if(a > b && a > c){
+    console.log("a é o maior número dos três!")
+} else if (b > a && b > c){
+    console.log("b é o maior número dos três!")
+} else {
+    console.log("c é o maior números dos três!")
+}*/
+
+/*4)Faça um programa que, dado um valor definido numa constante, retorne "positive" 
+se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.*/
+
+/*if(a > 0){
+    console.log("Número positivo!");
+} else if(a < 0){
+    console.log("Número negativo");
+} else {
+    console.log("Esse número é igual a 0!")
+}*/
+
+//5)Faça um programa que defina três constantes com os valores dos três ângulos 
+//internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. 
+//Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+/*if(a + b + c == 180 && a > 0 && b > 0 && c > 0){
+    console.log("Triângulo válido!")
+} else if(a + b + c > 180 || a + b + c < 180){
+    console.log("Triângulo inválido!")
+} else if(a <= 0 || b <= 0 || c <= 0){
+    console.log("Error 404!")
+}*/
+
+//6)Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+
+//7)
+
+let nota = 250;
+
+if(nota >= 90 && nota < 101){
+    console.log("A")
+}else if ( nota >= 80 && nota < 90){
+    console.log("B");
+}else if(nota >= 70 && nota < 80){
+    console.log('C');
+}else if(nota >= 60 && nota < 70){
+    console.log('D');
+}else if(nota >= 50 && nota < 60){
+    console.log('E');  
+}else if(nota < 50 && nota >= 0){
+    console.log('F');
+} else if (nota < 0 || nota > 100){
+    console.log('Error');
+}
+
+
