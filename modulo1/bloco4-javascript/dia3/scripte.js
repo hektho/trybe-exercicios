770e7e2b711d4c00ce48d0d9c650cc522706fab5
Heitor da Costa <heitor2000costa@gmail.com>
2022-02-05 11:58:16 -0300

    adicionado mais alguns exercicios concluidos

---
+++ modulo1/bloco4-javascript/dia3/scripte.js
@@ -0,0 +1,31 @@
+//1) Crie um algoritmo que retorne o fatorial de 10.
+
+/*let fat = 1;
+
+for(let index = 10; index >= 1; index-=1){
+    fat = fat * index
+}
+
+console.log(fat);*/
+
+//2) Escreva dois algoritmos: um que retorne a maior palavra deste array
+//e outro que retorne a menor.
+
+ /*let array = ['java', 'javascript','python','html','css'];
+ let menor = '';
+ let maior = '';
+ for(let index = 0; index < array.length; index+=1){
+    if(array[index].length > maior.length || maior.length === 0){
+        maior = array[index];
+    }
+ }
+for(let index = 0; index < array.length; index += 1){
+    if(array[index].length < menor.length || menor.length === 0){
+        menor = array[index]
+    }
+}
+console.log(menor)
+console.log(maior)*/
+
+
+
