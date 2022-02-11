d0f875cf9bc93fba29c71885aa285211a5bbf1b5
Heitor da Costa <heitor2000costa@gmail.com>
2022-02-10 21:39:02 -0300

    feito alguns exercicios sobre criacao de elementos HTML atraves de Javascript

---
+++ modulo1/bloco5-dom/dia2/script.js
@@ -0,0 +1,68 @@
+let body = document.querySelector('body');
+let h1 = document.createElement('h1');
+body.appendChild(h1);
+h1.innerText = 'Exercício 5.2 - JavaScript'
+
+
+
+let main = document.createElement('main');
+main.className = 'main-content';
+body.appendChild(main);
+
+
+
+let section = document.createElement('section');
+section.className = 'center-content';
+main.appendChild(section);
+
+
+
+let p = document.createElement('p');
+section.appendChild(p);
+p.innerText = 'ESTOU NASCENDO!!!! DO PÓ';
+
+
+let section1 = document.createElement('section');
+section1.className = 'left-content';
+main.appendChild(section1);
+
+let section2 = document.createElement('section');
+section2.className = 'right-content';
+main.appendChild(section2);
+
+let img = document.createElement('img');
+img.src = 'https://picsum.photos/200';
+img.className = 'small-image';
+section1.appendChild(img);
+
+let ul = document.createElement('ul')
+section2.appendChild(ul);
+//Criação de um array com o conteúdo da lista não-ordenada.
+let lista = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez']
+//Criação de uma função que adiciona este array em forma de lista dinamicamente.
+function createLi() {
+    for (let index = 0; index < lista.length; index += 1) {
+        //Criação de uma variável que contém a criação de um elemento 'li'.
+        let li = document.createElement('li');
+        //Este elemento 'li' será inserido em seguida ao elemento 'ul'
+        //como filho ao mesmo tempo que adiciona o valor contido no atual índice do array.
+        ul.appendChild(li).innerText = lista[index]
+    }
+
+}
+createLi();
+//Criação de uma função que recebe dois parâmetros, um número e uma string.
+//A função cria, de acordo com o parâmetro 'numero', uma quantidade de elemento 'h3' no site.
+function createSub(numero, nome) {
+    //A quantidade de loopings será limitado ao parâmetro numero definido pela pessoa.
+    for (let index = 0; index < numero; index += 1) {
+        let subtitulo = document.createElement('h3');
+        // Ao mesmo tempo que o elemento é criado como filho da tag 'main', um texto é adicionado que é o segundo parâmetro.
+        main.appendChild(subtitulo).innerText = nome;
+    }
+}
+
+createSub(3, 'Heitor');
+
+
+
