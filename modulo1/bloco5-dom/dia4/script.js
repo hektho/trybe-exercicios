ff3e06ae9482d90416732a73cc1df60d64968506
Heitor da Costa <heitor2000costa@gmail.com>
2022-02-19 14:14:26 -0300

    adicionado novo bloco e exercicios

--- modulo1/bloco5-dom/dia4/script.js
+++ modulo1/bloco5-dom/dia4/script.js
@@ -3,12 +3,36 @@ let button = document.querySelector('#button');
 let caixaDeTexto = document.querySelector('#text-box');
 let conteudoCaixaDeTexto = caixaDeTexto.value;
 
-button.addEventListener("click", changeColor);
-
-function changeColor() {
-    let conteudoCaixaDeTexto = caixaDeTexto.value;
-    body.style.backgroundColor = conteudoCaixaDeTexto;
+function armazenaValor(color) {
+    
+    button.addEventListener('click', changeColor);
+    function changeColor() {
+        body.style.backgroundColor = color
+        localStorage.setItem('backgroundColor', JSON.stringify(color));
+        let backgroundColor = JSON.parse(localStorage.getItem('backgroundColor'));
+        
+    }
+    changeColor();
 }
-changeColor();
+armazenaValor(conteudoCaixaDeTexto);
 
-console.log(conteudoCaixaDeTexto)
+let button2 = document.querySelector('#button2');
+let textBox2 = document.querySelector('#text-box2');
+
+button2.addEventListener("click", mudaFonte);
+
+function mudaFonte() {
+    let conteudoCaixa2 = textBox2.value;
+    body.style.color = conteudoCaixa2;
+
+}
+mudaFonte();
+
+let button3 = document.querySelector('#button3');
+let textBox3 = document.querySelector('#text-box3')
+
+button3.addEventListener('click', changeFontSize);
+
+function changeFontSize() {
+body.style.fontSize = textBox3.value + "px";
+}
